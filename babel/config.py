b1439919b044a1417daf926be6a328f54021f118
Parth Acharya <1008parth@gmail.com>
2024-08-31 20:57:26 +0530

    Update config to include port, host, and database URI

--- babel/config.py
+++ babel/config.py
@@ -6,4 +6,7 @@ load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 SESSION_LIFETIME = timedelta(days = int(os.environ.get("SESSION_LIFETIME")))
 SECRET_KEY = os.environ.get("SESSION_KEY")
-TRACK_MODIFICATIONS : bool = os.environ.get("TRACK_MODIFICATIONS")
+DATABASE_URI = os.environ.get("DATABASE_URI")
+TRACK_MODIFICATIONS = os.environ.get("TRACK_MODIFICATIONS")
+PORT = os.environ.get("PORT")
+HOST = os.environ.get("HOST")
