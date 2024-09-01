3420cf2d8bbb297d22b037e5e8a1964fd4b16be2
Parth Acharya <1008parth@gmail.com>
2024-09-01 15:24:36 +0530

    Add javascript for loading languages into translation page

---
+++ babel/static/scripts/load_languages.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", async function (event) {
+    try {
+        const response = await fetch("/fetch-languages", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if(!response.ok){
+            const statusCode = response.status;
+            const statusText = response.statusText;
+            throw new Error(`Failed to fetch available languages from server. Status: ${statusCode} ${statusText}`);
+        }
+
+        const data = await response.json();
+        const available_languages = data["lang"];
+        console.log(available_languages)
+        let languages_lists = document.querySelectorAll(".language-list");
+
+        languages_lists.forEach(languages_list => {
+            languages_list.innerHTML = "";
+
+            const is_destination_lang = languages_list.id === "dest-language";
+            Object.entries(available_languages).forEach(([key, value], index) => {
+                let lang = document.createElement("option");
+                if(is_destination_lang && key === "auto"){
+                    return;
+                }
+                else{
+                    lang.value = key;
+                    lang.text = key + " - " + value;
+                    lang.id = `${index+1}`;
+        
+                    languages_list.appendChild(lang);
+                }
+            });
+        })
+    }
+
+    catch(error){
+        console.error("Error: " + error.message)
+    }
+});
