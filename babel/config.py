305f9592d2da44f7ce10a9e774e288c74de613dc
Parth Acharya <1008parth@gmail.com>
2024-09-01 15:27:22 +0530

    Add constant AVAILABLE_LANGAUGES : dict to store supported languages

--- babel/config.py
+++ babel/config.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 import os
 from dotenv import load_dotenv
-
+import json
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 SESSION_LIFETIME = timedelta(days = int(os.environ.get("SESSION_LIFETIME")))
@@ -10,3 +10,6 @@ DATABASE_URI = os.environ.get("DATABASE_URI")
 TRACK_MODIFICATIONS = os.environ.get("TRACK_MODIFICATIONS")
 PORT = os.environ.get("PORT")
 HOST = os.environ.get("HOST")
+
+with open(os.environ.get("AVAILABLE_LANGUAGES"), "r") as languages_filepath:
+    AVAILABLE_LANGUAGES = json.load(languages_filepath)
