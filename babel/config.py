eb625beb00a8a0a5c4a06c78de47cd816012bf5a
Parth Acharya <1008parth@gmail.com>
2024-09-04 19:23:51 +0530

    Refactors constants into discrete objects based on their usage
    No major changes in constants, simple encapsulation only

--- babel/config.py
+++ babel/config.py
@@ -4,18 +4,24 @@ from dotenv import load_dotenv
 import json
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
-SESSION_LIFETIME = timedelta(days = int(os.environ.get("SESSION_LIFETIME")))
-SECRET_KEY = os.environ.get("SESSION_KEY")
-DATABASE_URI = os.environ.get("DATABASE_URI")
-TRACK_MODIFICATIONS = os.environ.get("TRACK_MODIFICATIONS")
-PORT = os.environ.get("PORT")
-HOST = os.environ.get("HOST")
+class Flask_Config:
+    """Base configuration."""
+    SECRET_KEY = os.environ.get("SESSION_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    TRACK_MODIFICATIONS = os.environ.get("TRACK_MODIFICATIONS")
+    PORT = os.environ.get("PORT")
+    HOST = os.environ.get("HOST")
+    SESSION_LIFETIME = timedelta(days=int(os.environ.get("SESSION_LIFETIME")))
+
+class AssemblyAI_Config:
+    """AssemblyAI configuration."""
+    UPLOAD_URL = 'https://api.assemblyai.com/v2/upload'
+    TRANSCRIPT_URL = 'https://api.assemblyai.com/v2/transcript'
+    AAI_API_KEY = os.environ.get("ASSEMBLY_AI_API_KEY")
 
 #Translation
 with open(os.environ.get("AVAILABLE_LANGUAGES"), "r") as languages_filepath:
     AVAILABLE_LANGUAGES = json.load(languages_filepath)
 
-# Transcription
-UPLOAD_URL = 'https://api.assemblyai.com/v2/upload'
-TRANSCRIPT_URL = 'https://api.assemblyai.com/v2/transcript'
-AAI_API_KEY = os.environ.get("ASSEMBMLY_AI_API_KEY")
+flask_config = Flask_Config()
+aai_config = AssemblyAI_Config()
