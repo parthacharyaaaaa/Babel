770ad74741c30916830b20958b6c811c2ae7aa4c
Parth Acharya <1008parth@gmail.com>
2024-09-04 19:25:47 +0530

    Misc changes

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from babel import app, db
-from babel.config import PORT, HOST
+from babel.config import flask_config
+
+HOST = flask_config.HOST
+PORT = flask_config.PORT
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
