39e4e85942a042234a5a265874ab07df1a70d429
Parth Acharya <1008parth@gmail.com>
2024-09-07 22:47:31 +0530

    Updates logic for file validation and displaying response

--- babel/static/scripts/transcript.js
+++ babel/static/scripts/transcript.js
@@ -1,10 +1,34 @@
 document.addEventListener("DOMContentLoaded", async function(event) {
+    const audio_dropper = document.getElementById("audio-dropper");
+    const audio_label = document.getElementById("audio-label").querySelector("span");
+
+    audio_dropper.addEventListener("change", function(event){
+        const fileName = audio_dropper.files[0]?.name || "Upload Audio File";
+        audio_label.innerText = fileName;
+    })
+
     document.getElementById("submit-audio").addEventListener("click", async function(event){
         try{
+            if(document.getElementById("audio-dropper").files.length !== 1){
+                throw new Error("Only one file can be processed at a time")
+            }
+
+            let output_box = document.getElementById("transcription-output");
+            let confidence_box = document.getElementById("confidence");
+            output_box.innerText = "";
+            confidence_box.innerText = "";
+            
             const file = document.getElementById("audio-dropper").files[0];
             if (!file){
                 throw new Error("File Not Found :(");
             }
+
+            const extension = file.name.split(".").pop().toLowerCase();
+            if(!["mp3", "aac", "wav", "ogg"].includes(extension)){
+                alert("Invalid file extension chosen");
+                return;
+            }
+
             console.log("Selected File: " + file)
             let audioForm = new FormData();
             audioForm.append("audio-file", file);
@@ -26,10 +50,9 @@ document.addEventListener("DOMContentLoaded", async function(event) {
                 throw new Error("Unexpected response format :(");
             }
 
-            let output_box = document.getElementById("transcription-output");
-            let confidence_box = document.getElementById("confidence");
+
             output_box.innerText = transcript;
-            confidence_box.innerText = confidence;
+            confidence_box.innerText = `Confidence: ${(confidence*100).toFixed(2)}%`;
         }
         catch (error){
             console.error("Error ", error);
