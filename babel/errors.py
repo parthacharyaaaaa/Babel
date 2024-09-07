aa2d574a70714fd82e2ed1f57a2a8bd8a9badb3a
Parth Acharya <1008parth@gmail.com>
2024-09-07 22:50:00 +0530

    Adds API_TIMEOUT_ERROR, Missing_Configuration_Error & Unexpected_Response_Format. Removes Optional from type hints for message in __init__ of all classes

--- babel/errors.py
+++ babel/errors.py
@@ -1,4 +1,19 @@
 class Unexpected_Request_Format(Exception):
-    def __init__(self, message : str | None = "Given request is of unexpected format") -> None:
+    def __init__(self, message : str = "Given request is of unexpected format") -> None:
         self.message = message
+        super.__init__(self, self.message)
+
+class Unexpected_Response_Format(Exception):
+    def __init__(self, message : str = "Given response is of unexpected format") -> None:
+        self.message = message
+        super.__init__(self, self.message)
+
+class Missing_Configuration_Error(Exception):
+    def __init__(self, message : str = "Failure in loading configurations") -> None:
+        self.message = message
+        super.__init__(self, self.message)
+
+class API_TIMEOUT_ERROR(Exception):
+    def __init__(self, endpoint : str, message : str = "Timeout reached with API Endpoint: {}") -> None:
+        self.message = message.format(endpoint)
         super.__init__(self, self.message)
