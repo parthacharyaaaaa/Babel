0f9dd97e39c979afdf35c6eca9e41da7987a45dd
Parth Acharya <1008parth@gmail.com>
2024-10-31 13:03:40 +0530

    Adds auth app factory

--- babel_auth/__init__.py
+++ babel_auth/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from babel_auth.config import flaskconfig
 
 auth = Flask(__name__)
-
+auth.config.from_object(flaskconfig)
 db = SQLAlchemy(auth)
 migrate = Migrate(auth, db)
 
