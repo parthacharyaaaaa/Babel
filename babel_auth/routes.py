6f81871346ddf3c9ef6e1743512d8d56b9ec6804
Parth Acharya <1008parth@gmail.com>
2024-11-08 20:19:19 +0530

    Adds logic for token issuance in /LOGIN

--- babel_auth/routes.py
+++ babel_auth/routes.py
@@ -1,10 +1,27 @@
 from babel_auth import auth, tokenManager
 from flask import request, abort, jsonify, Response
-from werkzeug.exceptions import BadRequestKeyError, BadRequest
+from werkzeug.exceptions import BadRequest
+from datetime import datetime
 
 @auth.route("/login", methods = ["POST"])
 def login():
-    return jsonify("bitches ain't shit but hoes and tricks"), 200
+    if not request.is_json:
+        raise BadRequest()
+    authentication_data = request.get_json()
+    if not ("identity" in authentication_data and "password" in authentication_data):
+        raise BadRequest()
+    
+    aToken = tokenManager.issueAccessToken()
+    rToken = tokenManager.issueRefreshToken(familyID=1)
+
+    response = jsonify({
+        "access" : aToken,
+        "refresh" : rToken,
+        "time_of_issuance" : datetime.now(),
+        "issuer" : "babel-auth-service"
+    })
+
+    return response, 201
 
 @auth.route("/signup", methods = ["POST"])
 def signup():
