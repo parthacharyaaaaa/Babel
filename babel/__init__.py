d2a548d7fe99eb012cb12cd18c03360aee3643f5
Parth Acharya <1008parth@gmail.com>
2024-12-14 17:34:27 +0530

    Adds Redis setup

--- babel/__init__.py
+++ babel/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import redis
 
 from babel.config import flask_config
 
@@ -11,6 +12,8 @@ app.config.from_object(flask_config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
+
+REDIS_INTERFACE = redis.Redis(host="localhost", port=1234, db = 0, health_check_interval = 60)
 
 from babel import routes
 from babel import models
