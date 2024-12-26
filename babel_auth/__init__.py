711232740276b0a486a7b314080d73d576e5e419
Parth Acharya <1008parth@gmail.com>
2024-12-26 12:30:18 +0530

    Fixes CORS rule

--- babel_auth/__init__.py
+++ babel_auth/__init__.py
@@ -9,7 +9,7 @@ from babel_auth.schema import TokenManager
 auth = Flask(__name__)
 auth.config.from_object(flaskconfig)
 print(f"{auth.config['PROTOCOL']}://{auth.config['RESOURCE_SERVER_ORIGIN']}")
-cors = CORS(auth, resources={r"/" : {"origins" : [f"{auth.config['PROTOCOL']}://{auth.config['RESOURCE_SERVER_ORIGIN']}"]}})
+cors = CORS(auth, resources={r"/*" : {"origins" : [f"{auth.config['PROTOCOL']}://{auth.config['RESOURCE_SERVER_ORIGIN']}"]}})
 
 # Set up token manager
 with open(os.path.join(CWD, os.environ["ACCESS_SCHEMA_FP"]), "r") as accessSchema:
@@ -17,7 +17,6 @@ with open(os.path.join(CWD, os.environ["ACCESS_SCHEMA_FP"]), "r") as accessSchem
 with open(os.path.join(CWD, os.environ["REFRESH_SCHEMA_FP"]), "r") as refreshSchema:
     refreshDict : dict = json.load(refreshSchema)
 tokenManager = TokenManager(signingKey=auth.config["SIGNING_KEY"],
-                            connString=os.path.join(CWD, os.environ["AUTH_DB_URI"]),
                             accessSchema=accessDict,
                             refreshSchema=refreshDict)
 
