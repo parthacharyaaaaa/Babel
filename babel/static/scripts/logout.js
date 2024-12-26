1d44dd05a2a78fe936a83f8e3ff6367450c295b5
Parth Acharya <1008parth@gmail.com>
2024-12-26 20:02:35 +0530

    Adds API calling to GET /purge-family to enable logout via client

---
+++ babel/static/scripts/logout.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    const logoutBtn = document.getElementById("logout-btn");
+    logoutBtn.addEventListener("click", async function(event){
+        try{
+            const response = await fetch(`http://${window.location.hostname}:8080/purge-family`, {
+                headers : {
+                    "Content-Type" : "application/json"
+                },
+                method : "GET",
+                credentials : "include"
+            });
+
+            if (!response.ok){
+                throw new Error(`${response.status}: ${response.statusText}`)
+            }
+
+            alert("You have logged out successfully");
+            window.location.href = "/";
+
+        }
+        catch(error){
+            console.error(error)
+        }
+
+    })
+})
