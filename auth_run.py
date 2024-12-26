b7d4a211684fb434fe781b52e23c0d357d27df8e
Parth Acharya <1008parth@gmail.com>
2024-12-26 12:30:43 +0530

    Removes SQLite operations

--- auth_run.py
+++ auth_run.py
@@ -1,8 +1,3 @@
-from babel_auth import auth, db
-from babel_auth.models import Token
-
+from babel_auth import auth
 if __name__ == "__main__":
-    with auth.app_context():
-        db.create_all()
-    
     auth.run(host=auth.config["HOST"], port = auth.config["PORT"], debug=True)
