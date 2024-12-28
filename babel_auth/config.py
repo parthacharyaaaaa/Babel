a6840e018e90dd0794204a15b9f04359f0089901
Parth Acharya <1008parth@gmail.com>
2024-12-28 12:18:04 +0530

    Updates  auth.config

--- babel_auth/config.py
+++ babel_auth/config.py
@@ -7,22 +7,29 @@ load_dotenv(os.path.join(CWD, ".env"), override=True)
 
 class FlaskConfig:
     try:
-        # Security Metadata
+        # Security metadata
         SECRET_KEY = os.environ["SECRET_KEY"]
         SIGNING_KEY = os.environ["SIGNING_KEY"]
         SESSION_COOKIE_SECURE = bool(os.environ["SESSION_COOKIE_SECURE"])
+        PRIVATE_COMM_KEYS : list = os.environ["PRIVATE_COMM_KEYS"].split(",")
+        CSP = f"default-src 'self'; connect-src 'self' {os.environ['RS_DOMAIN']}"
 
-        # Database Metadata
+        # IP metadata
+        VALID_PROXIES : list = os.environ["VALID_PROXIES"].split(",")
+        PRIVATE_IP_ADDRS : list = os.environ["PRIVATE_IP_ADDRS"].split(",")
+
+        # Database metadata
         SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(CWD, os.environ["AUTH_DB_URI"])
         SQLALCHEMY_TRACK_MODIFICATIONS = bool(os.environ.get("TRACK_MODIFICATIONS", False))
 
-        CSP = f"default-src 'self'; connect-src 'self' {os.environ['RS_DOMAIN']}"
-
-        # Deployment Metadata
+        # Addressing metadata
         PORT = int(os.environ["PORT"])
         HOST = os.environ["HOST"]
+
+        # Resource server metadata
         RESOURCE_SERVER_ORIGIN = os.environ["RS_DOMAIN"].lower()
         PROTOCOL = os.environ.get("RS_COMMUNICATION_PROTOCOL", "http").lower()
+
     except KeyError as e:
         raise Missing_Configuration_Error(f"FAILED TO SETUP CONFIGURATIONS FOR FLASK AUTH APPLICATION AS ENVIRONMENT VARIABLES WERE NOT FOUND (SEE: class Flask_Config at '{__file__}')")
     except TypeError as e:
